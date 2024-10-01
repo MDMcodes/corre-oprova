18f688800f4ade7747f1a6b3a58a86b9a4470418
MDMcodes <matheusdonadelmarques08@gmail.com>
2024-10-01 20:13:17 -0300

    commit03

--- ex01.py
+++ ex01.py
@@ -9,7 +9,11 @@ while True:
     opc = input("Adicionar mais?:  (s/n) ").lower()
     if opc == "n":
         break
-    
+
 #receitas = a, despesas = b
 lucromensal = list(map(lambda a, b: a - b, receitas, despesas))
+print(lucromensal)
+media_anual = sum(lucromensal) #sum = soma de tudo q esta dentro da lista
+print(media_anual / len(lucromensal)) #len = pega a quantidade de elementos dentro da lista
+
 
